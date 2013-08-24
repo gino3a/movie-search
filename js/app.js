99da00badb8b37fe7221dd4c308d99cfc0424ffc
gino <romanangelo.tria@gmail.com>
2013-08-24 08:09:29 +0800

    show movie details on hover

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function() {
 
+    var app = {};
     var url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json';
     $.ajax({
         url: url,
@@ -9,14 +10,26 @@ $(function() {
         dataType: 'jsonp',
         success: showBoxOffice
     });
+    function getTemplate(template_id, context) {
+        var template, $template, markup;
+        template = $('#' + template_id);
+        $template = Handlebars.compile(template.html());
+        markup = $template(context);
+        return markup;
+
+    }
     function showBoxOffice(response) {
+        app.movies = response.movies;
         var movie, template, $template, markup;
-        for (var i = 0; i < response.movies.length; i++) {
-                movie = response.movies[i];
-                template = $('#tpl-box-office-item');
-                $template = Handlebars.compile(template.html());
-                markup = $template(movie);
-                $('table tbody').append(markup);
+        for (var i = 0; i < app.movies.length; i++) {
+                movie = app.movies[i];
+                movie._index = i;
+                $('table tbody').append(getTemplate('tpl-box-office-item', movie));
         }
+        $('table tbody>tr').hover(function(ev) {
+            var data = $(ev.target).closest('tr').data();
+            var movie = app.movies[data.id];
+            $('.movie-detail').html(getTemplate('tpl-movie-detail', movie));
+        });
     }
 });
